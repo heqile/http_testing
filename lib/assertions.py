3c5ed3f829cca7a22a48054b25f06f0ba546483c
heqile <mr.qile@gmail.com>
2022-11-10 00:15:28 +0100

    feat: use regex in asserts

--- lib/assertions.py
+++ lib/assertions.py
@@ -1,3 +1,4 @@
+import re
 from dataclasses import dataclass
 from typing import Mapping, Optional, Sequence
 
@@ -13,14 +14,18 @@ class Assertions:
     def check_assertions(self, response: Response, negative: bool = False) -> bool:
         if self.content:
             for content in self.content:
-                if (content not in response.text) ^ negative:
+                if (re.search(content, response.text, re.MULTILINE) is None) ^ negative:
                     return False
         if self.headers:
             for header_key, header_value in self.headers.items():
-                if (not response.headers.get(header_key) == header_value) ^ negative:
+                if (
+                    header_key not in response.headers or re.search(header_value, response.headers[header_key]) is None
+                ) ^ negative:
                     return False
         if self.cookies:
             for cookie_key, cookie_value in self.cookies.items():
-                if (not response.cookies.get(cookie_key) == cookie_value) ^ negative:
+                if (
+                    cookie_key not in response.cookies or re.search(cookie_value, response.cookies[cookie_key]) is None
+                ) ^ negative:
                     return False
         return True
