f7027c0f785fe2a93645e3819ba4d721e7e44d50
heqile <mr.qile@gmail.com>
2022-11-11 15:53:55 +0100

    chore: review typig

--- lib/assertion_elements/assertion_base.py
+++ lib/assertion_elements/assertion_base.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Any, Dict, Type
+from typing import Dict, List, Type
 
 from httpx import Client, Response
 
@@ -8,7 +8,7 @@ from .assert_element_base import AssertElementBase
 
 class AssertionBase:
     _assert_type: Type[AssertElementBase]
-    assertion_instances: Dict[int, Any] = defaultdict(list)
+    assertion_instances: Dict[int, List[AssertElementBase]] = defaultdict(list)
 
     def __get__(self, instance, owner):
         if not hasattr(instance, self._private_name):
@@ -25,6 +25,5 @@ class AssertionBase:
 
 
 def check_all(instance, http_client: Client, response: Response, negative: bool):
-    assertion_instance: AssertElementBase
     for assertion_instance in AssertionBase.assertion_instances[id(instance)]:
         assertion_instance.check(http_client, response, negative)
