43610ad8dcf1df6f82f0cc1436939b77df748632
heqile <mr.qile@gmail.com>
2022-11-12 15:04:43 +0100

    test: add cookie checker when negative

--- test_core/test_cookies_assertion.py
+++ test_core/test_cookies_assertion.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 from http.cookiejar import Cookie as HttpCookie
 from http.cookiejar import CookieJar
 from unittest import mock
@@ -39,44 +40,102 @@ class CookieSuite:
         return cookie
 
 
-def test_check_should_not_raise_with_same_cookie_name_on_multiple_sites():
+@pytest.mark.parametrize(
+    "negative, expected_raise",
+    [
+        pytest.param(
+            False,
+            nullcontext(),
+            id="should_not_raise",
+        ),
+        pytest.param(
+            True,
+            pytest.raises(AssertionError),
+            id="should_raise_when_negative_is_true",
+        ),
+    ],
+)
+def test_check_with_same_cookie_name_on_multiple_sites(negative, expected_raise):
     mock_client = mock.MagicMock(spec=Client, auto_spec=True)
     mock_client.cookies.jar = CookieJar()
     mock_client.cookies.jar.set_cookie(CookieSuite.cookie_A())
     mock_client.cookies.jar.set_cookie(CookieSuite.cookie_A_on_another_site())
     mock_response = mock.MagicMock(spec=Response, auto_spec=True)
-    _CookiesChecker(value=[Cookie(name="cookie_one", value_pattern="cookie_value_one", domain="site_one")]).check(
-        http_client=mock_client, response=mock_response, negative=False
-    )
+    checker = _CookiesChecker(value=[Cookie(name="cookie_one", value_pattern="cookie_value_one", domain="site_one")])
+    with expected_raise:
+        checker.check(http_client=mock_client, response=mock_response, negative=negative)
 
 
-def test_check_should_raise_with_not_cookie_match_on_same_site():
+@pytest.mark.parametrize(
+    "negative, expected_raise",
+    [
+        pytest.param(
+            False,
+            pytest.raises(AssertionError),
+            id="should_raise",
+        ),
+        pytest.param(
+            True,
+            nullcontext(),
+            id="should_not_raise_when_negative_is_true",
+        ),
+    ],
+)
+def test_check_with_not_cookie_match_on_same_site(negative, expected_raise):
     mock_client = mock.MagicMock(spec=Client, auto_spec=True)
     mock_client.cookies.jar = CookieJar()
     mock_client.cookies.jar.set_cookie(CookieSuite.cookie_A_on_another_site())
     mock_response = mock.MagicMock(spec=Response, auto_spec=True)
-    assert_element = _CookiesChecker(
-        value=[Cookie(name="cookie_one", value_pattern="cookie_value_one", domain="site_one")]
-    )
-    with pytest.raises(AssertionError):
-        assert_element.check(http_client=mock_client, response=mock_response, negative=False)
+    checker = _CookiesChecker(value=[Cookie(name="cookie_one", value_pattern="cookie_value_one", domain="site_one")])
+    with expected_raise:
+        checker.check(http_client=mock_client, response=mock_response, negative=negative)
 
 
-def test_check_should_raise_when_secure_not_match():
+@pytest.mark.parametrize(
+    "negative, expected_raise",
+    [
+        pytest.param(
+            False,
+            pytest.raises(AssertionError),
+            id="should_raise",
+        ),
+        pytest.param(
+            True,
+            nullcontext(),
+            id="should_not_raise_when_negative_is_true",
+        ),
+    ],
+)
+def test_check_when_secure_not_match(negative, expected_raise):
     mock_client = mock.MagicMock(spec=Client, auto_spec=True)
     mock_client.cookies.jar = CookieJar()
     mock_client.cookies.jar.set_cookie(CookieSuite.cookie_A())
     mock_response = mock.MagicMock(spec=Response, auto_spec=True)
     assert_element = _CookiesChecker(value=[Cookie(name="cookie_one", value_pattern="cookie_value_one", secure=True)])
-    with pytest.raises(AssertionError):
-        assert_element.check(http_client=mock_client, response=mock_response, negative=False)
+    with expected_raise:
+        assert_element.check(http_client=mock_client, response=mock_response, negative=negative)
 
 
-def test_check_should_raise_when_expiration_not_match():
+@pytest.mark.parametrize(
+    "negative, expected_raise",
+    [
+        pytest.param(
+            False,
+            pytest.raises(AssertionError),
+            id="should_raise",
+        ),
+        pytest.param(
+            True,
+            nullcontext(),
+            id="should_not_raise_when_negative_is_true",
+        ),
+    ],
+)
+def test_check_should_raise_when_expiration_not_match(negative, expected_raise):
     mock_client = mock.MagicMock(spec=Client, auto_spec=True)
     mock_client.cookies.jar = CookieJar()
     mock_client.cookies.jar.set_cookie(CookieSuite.cookie_A())
     mock_response = mock.MagicMock(spec=Response, auto_spec=True)
     assert_element = _CookiesChecker(value=[Cookie(name="cookie_one", value_pattern="cookie_value_one", expires=1)])
-    with pytest.raises(AssertionError):
-        assert_element.check(http_client=mock_client, response=mock_response, negative=False)
+    with expected_raise:
+        assert_element.check(http_client=mock_client, response=mock_response, negative=negative)
