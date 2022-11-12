94fb1d04e3713b9aeae2e218ad91d96389f8c71f
heqile <mr.qile@gmail.com>
2022-11-12 14:12:29 +0100

    feat: use WeakKeyDictionary instead of finalize

--- lib/assertion_elements/assertion_base.py
+++ lib/assertion_elements/assertion_base.py
@@ -1,6 +1,5 @@
-from collections import defaultdict
-from typing import Dict, List, Type
-from weakref import finalize
+from typing import Type
+from weakref import WeakKeyDictionary
 
 from httpx import Client, Response
 
@@ -9,7 +8,9 @@ from .assert_element_base import AssertElementBase
 
 class AssertionBase:
     _assert_type: Type[AssertElementBase]
-    assertion_instances: Dict[int, List[AssertElementBase]] = defaultdict(list)
+    # if the owner instance of the descriptor is deleted, we should also remove the its entry from assertion_instances
+    # that's why we use WeakKeyDictionary
+    assertion_instances: WeakKeyDictionary = WeakKeyDictionary()
 
     def __get__(self, instance, owner):
         if not hasattr(instance, self._private_name):
@@ -21,20 +22,13 @@ class AssertionBase:
 
     def __set__(self, instance, value):
         assertion_instance = self._assert_type(value)
-        dict_key = get_dict_key(instance)
-        self.assertion_instances[dict_key].append(assertion_instance)
+        if instance not in self.assertion_instances:
+            self.assertion_instances.setdefault(instance, [])
+        self.assertion_instances[instance].append(assertion_instance)
         setattr(instance, self._private_name, assertion_instance)
-        finalize(instance, self._remove_assertion_instance, dict_key)  # remove the entry when instance is deleted
-
-    def _remove_assertion_instance(self, key):
-        if key in self.assertion_instances:
-            del self.assertion_instances[key]
-
-
-def get_dict_key(instance):
-    return id(instance)
 
 
 def check_all(instance, http_client: Client, response: Response, negative: bool):
-    for assertion_instance in AssertionBase.assertion_instances[get_dict_key(instance)]:
+    assertion_instance: AssertElementBase
+    for assertion_instance in AssertionBase.assertion_instances[instance]:
         assertion_instance.check(http_client, response, negative)
